889cb29d0358b9e45555086e8dadf6faf666ee73
REMANTH <hulk102700@gmail.com>
2023-05-31 11:38:24 +0530

    this is project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { FAQComponent } from './faq/faq.component';
+import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent,
+    FAQComponent,
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
